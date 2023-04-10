61d5d6d98bd187ee60f3eb2e269ed81637404847
A-Gorby <123462010+A-Gorby@users.noreply.github.com>
2023-04-10 20:07:28 +0300

    Add files via upload

---
+++ utils_esklp.py
@@ -0,0 +1,83 @@
+import os
+import sys
+import glob
+import logging
+import zipfile
+import datetime
+import pandas as pd
+
+from utils_io import logger
+if len(logger.handlers) > 1:
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
+    from utils_io import logger
+
+def find_last_fn_pickle(prefix, path_files):
+    fn_pickle = None
+    if prefix is None: prefix =''
+    # fn_list = sorted(glob.glob(os.path.join(path_files, prefix) + '*.pickle'))
+    # fn_list = sorted(glob.glob(path_files + prefix + '*.pickle'))
+    fn_list = sorted(glob.glob(os.path.join(path_files, prefix + '*.pickle')))
+    # print(fn_list)
+    if len(fn_list)>0:  fn_pickle = fn_list[-1]
+    return fn_pickle
+    
+def restore_df_from_pickle(prefix, path_files, fn_pickle):
+    # print(f"restore_df_from_pickleЖ prefix: '{prefix}', path_files: '{path_files}', fn_pickle: '{fn_pickle}'")
+    if fn_pickle == 'last':
+        # fn_pickle = 'smnn_list_v2022_09_23.pickle'
+        #smnn_list_df_esklp
+        fn_pickle = find_last_fn_pickle(prefix, path_files = path_files)
+    elif fn_pickle is not None:
+        pass
+        # fn_pickle = fn_pickle
+    else:
+        fn_pickle = find_last_fn_pickle(prefix = prefix, path_files = path_files)
+    # print(f"restore_df_from_pickle: fn_pickle: {fn_pickle}")
+    if fn_pickle is None:
+        logger.error('Restore pickle from ' + path_files + ' failed!')
+        sys.exit(2)
+    if os.path.exists(os.path.join(path_files, fn_pickle)):
+        df = pd.read_pickle(os.path.join(path_files, fn_pickle))
+        logger.info('Restore ' + fn_pickle + ' done!')
+    else:
+        logger.error('Restore ' + fn_pickle + ' from ' + path_files + ' failed!')
+    return df
+    
+def unzip_esklp(path_esklp_source, fn_esklp_xml_active_zip, work_path):
+    logger.info('Unzip ' + fn_esklp_xml_active_zip + ' start...')
+
+    try:
+        # with zipfile.ZipFile(path_esklp_source + fn_esklp_xml_active_zip, 'r') as zip_ref:
+        with zipfile.ZipFile(os.path.join(path_esklp_source, fn_esklp_xml_active_zip), 'r') as zip_ref:
+            fn_list = zip_ref.namelist()
+            zip_ref.extractall(work_path)
+        logger.info('Unzip ' + fn_esklp_xml_active_zip + ' done!')
+        return fn_list[0]
+    except Exception as err:
+        logger.error('Unzip error: ' + str(err))
+        sys.exit(2)
+
+def unzip_file(path_source, fn_zip, work_path):
+    logger.info('Unzip ' + fn_zip + ' start...')
+
+    try:
+        # with zipfile.ZipFile(path_source + fn_zip, 'r') as zip_ref:
+        with zipfile.ZipFile(os.path.join(path_source, fn_zip), 'r') as zip_ref:
+            fn_list = zip_ref.namelist()
+            zip_ref.extractall(work_path)
+        logger.info('Unzip ' + fn_zip + ' done!')
+        return fn_list[0]
+    except Exception as err:
+        logger.error('Unzip error: ' + str(err))
+        sys.exit(2)
+
+def save_df_to_pickle(df, path_to_save, fn_main):
+    offset = datetime.timezone(datetime.timedelta(hours=3))
+    dt = datetime.datetime.now(offset)
+    str_date = dt.strftime("%Y_%m_%d_%H%M")
+    fn = fn_main + '_' + str_date + '.pickle'
+    # df.to_pickle(path_to_save + fn)
+    df.to_pickle(os.path.join(path_to_save, fn))
+    logger.info(fn + ' saved to ' + path_to_save)
+    return fn
---
+++ utils_form_esklp.py
@@ -0,0 +1,46 @@
+import ipywidgets as widgets
+from ipywidgets import Layout, Box, Label
+def form_param_esklp(fn_list):
+    fn_esklp_xml_zip_file_drop_douwn = widgets.Dropdown( options=fn_list, value=None) #fn_list[0] if len(fn_list) > 0 else None, disabled=False)
+    # fn_desc_file_drop_douwn = widgets.Dropdown( options=fn_list, value=None)
+    # sheet_name_drop_douwn = widgets.Dropdown( options= [None], value= None, disabled=False)
+    # col_name_drop_douwn = widgets.Dropdown( options= [None], value= None, disabled=False)
+    # fn_dict_file_drop_douwn = widgets.Dropdown( options= [None] + fn_list, value= None, disabled=False, )
+    # radio_btn_big_dict = widgets.RadioButtons(options=['Р”Р°', 'РќРµС‚'], value= 'Р”Р°', disabled=False) # description='Check me',    , indent=False
+    # radio_btn_prod_options = widgets.RadioButtons(options=['Р”Р°', 'РќРµС‚'], value= 'РќРµС‚', disabled=False if radio_btn_big_dict.value=='Р”Р°' else True )
+    # similarity_threshold_slider = widgets.IntSlider(min=1,max=100, value=90)
+    # max_entries_slider = widgets.IntSlider(min=1,max=5, value=4)
+    # max_out_values_slider = widgets.IntSlider(min=1,max=10, value=4)
+
+    form_item_layout = Layout(display='flex', flex_flow='row', justify_content='space-between')
+    check_box = Box([Label(value="Выберите xml.zip-файл со справочником ЕСКЛП:"), fn_esklp_xml_zip_file_drop_douwn], layout=form_item_layout) 
+    # desc_box = Box([Label(value='Выберите Excel-файл с описанием моделей:'), fn_desc_file_drop_douwn], layout=form_item_layout) 
+    # sheet_box = Box([Label(value='Выберите лист Excel-файла:'), sheet_name_drop_douwn], layout=form_item_layout) 
+    # column_box = Box([Label(value='Р—Р°РіРѕР»РѕРІРѕРє РєРѕР»РѕРЅРєРё:'), col_name_drop_douwn], layout=form_item_layout) 
+    # big_dict_box = Box([Label(value='РСЃРїРѕР»СЊР·РѕРІР°С‚СЊ Р±РѕР»СЊС€РёРµ СЃРїСЂР°РІРѕС‡РЅРёРєРё:'), radio_btn_big_dict], layout=form_item_layout) 
+    # prod_options_box = Box([Label(value='РСЃРєР°С‚СЊ РІ Р’Р°СЂРёР°РЅС‚Р°С… РёСЃРїРѕР»РЅРµРЅРёСЏ (+10 РјРёРЅ):'), radio_btn_prod_options], layout=form_item_layout) 
+    # similarity_threshold_box = Box([Label(value='РњРёРЅРёРјР°Р»СЊРЅС‹Р№ % СЃС…РѕРґСЃС‚РІР° РїРѕР·РёС†РёР№:'), similarity_threshold_slider], layout=form_item_layout) 
+    # max_entries_box = Box([Label(value='РњР°РєСЃРёРјР°Р»СЊРЅРѕРµ РєРѕР»-РІРѕ РЅР°Р№РґРµРЅРЅС‹С… РїРѕР·РёС†РёР№:'), max_entries_slider], layout=form_item_layout) 
+    # max_out_values_box = Box([Label(value='РњР°РєСЃРёРјР°Р»СЊРЅРѕРµ РєРѕР»-РІРѕ РІС‹РІРѕРґРёРјС‹С… РїРѕР·РёС†РёР№:'), max_out_values_slider], layout=form_item_layout) 
+    
+    # form_items = [check_box, dict_box, big_dict_box, prod_options_box, similarity_threshold_box, max_entries_box]
+    form_items = [check_box] #, column_box, similarity_threshold_box, max_entries_box, max_out_values_box]
+    
+    form_esklp = Box(form_items, layout=Layout(display='flex', flex_flow= 'column', border='solid 2px', align_items='stretch', width='50%')) #width='auto'))
+    # return form, fn_check_file_drop_douwn, fn_dict_file_drop_douwn, radio_btn_big_dict, radio_btn_prod_options, similarity_threshold_slider, max_entries_slider
+    return form_esklp, fn_esklp_xml_zip_file_drop_douwn
+
+def def_form_esklp_upd_unify_pharm_forms(pharm_forms_to_add, unify_pharm_forms):
+    form_item_layout = Layout(display='flex', flex_flow='row', justify_content='space-between')
+    
+    unify_pharm_forms_drop_douwn = widgets.Dropdown( options=unify_pharm_forms, value=None)
+    # check_box = Box([Label(value="Выберите xml.zip-файл со справочником ЕСКЛП:"), fn_esklp_xml_zip_file_drop_douwn], layout=form_item_layout) 
+    # check_box = Box([Label(value=pharm_forms_to_add[0]), unify_pharm_forms_drop_douwn], layout=form_item_layout)
+    
+    pre_value = [widgets.Dropdown( options=unify_pharm_forms, value=pharm_form.split()[0].capitalize() if pharm_form.split()[0].capitalize() in unify_pharm_forms else None) 
+        for pharm_form in pharm_forms_to_add] 
+    form_items = [Box([Label(value=pharm_form), pre_value[i_p]], layout=form_item_layout) for i_p, pharm_form in enumerate(pharm_forms_to_add)] 
+    
+    form_esklp_upd_unify_pharm_forms = Box(form_items, layout=Layout(display='flex', flex_flow= 'column', border='solid 2px', align_items='stretch', width='50%')) #width='auto'))
+    # return form, fn_check_file_drop_douwn, fn_dict_file_drop_douwn, radio_btn_big_dict, radio_btn_prod_options, similarity_threshold_slider, max_entries_slider
+    return form_esklp_upd_unify_pharm_forms, pre_value    

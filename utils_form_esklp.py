94c6e881225c4e377d9755b17164418b64a70867
A-Gorby <123462010+A-Gorby@users.noreply.github.com>
2023-04-14 18:40:15 +0300

    Add files via upload

--- utils_form_esklp.py
+++ utils_form_esklp.py
@@ -45,3 +45,14 @@ def def_form_esklp_upd_unify_pharm_forms(pharm_forms_to_add, unify_pharm_forms):
     form_esklp_upd_unify_pharm_forms = Box(form_items, layout=Layout(display='flex', flex_flow= 'column', border='solid 2px', align_items='stretch', width='50%')) #width='auto'))
     # return form, fn_check_file_drop_douwn, fn_dict_file_drop_douwn, radio_btn_big_dict, radio_btn_prod_options, similarity_threshold_slider, max_entries_slider
     return form_esklp_upd_unify_pharm_forms, pre_value    
+
+def form_param_esklp_exist_dicts(esklp_dates):
+    esklp_dates_dropdown = widgets.Dropdown( options=esklp_dates) #, value=None)
+    
+    form_item_layout = Layout(display='flex', flex_flow='row', justify_content='space-between')
+    check_box = Box([Label(value="Выберите дату сохраненного справочника ЕСКЛП:"), esklp_dates_dropdown], layout=form_item_layout) 
+    form_items = [check_box]
+    
+    form_esklp_exist_dicts = Box(form_items, layout=Layout(display='flex', flex_flow= 'column', border='solid 2px', align_items='stretch', width='50%')) #width='auto'))
+    # return form, fn_check_file_drop_douwn, fn_dict_file_drop_douwn, radio_btn_big_dict, radio_btn_prod_options, similarity_threshold_slider, max_entries_slider
+    return form_esklp_exist_dicts, esklp_dates_dropdown 
